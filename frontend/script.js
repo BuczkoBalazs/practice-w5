a30931f866d0961cce334bcf3c868e8cef8ed212
BuczkoBalazs <buci85@gmail.com>
2022-02-16 18:21:22 +0100

    vége

--- frontend/script.js
+++ frontend/script.js
@@ -168,7 +168,24 @@ window.addEventListener("load", function() {
 
 function loadEvent() {
     console.log("My second function")
-}
+
+    let rootElement = document.getElementById("root");
+
+    let card = function (movieReceived){
+    
+        return `
+        <div class="card">
+            <h2>${movieReceived.title}</h2>
+            <div class="time">${movieReceived.year}</div>
+            <div class="rate">${movieReceived.rate}</div>
+        </div>
+        `;
+    };
+
+    for (const movieSend of movies) {
+        rootElement.insertAdjacentHTML("beforeend", card(movieSend));
+    };
+};
 window.addEventListener("load", loadEvent);
 
 
