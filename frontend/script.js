ef014702b79e60eebdae784eb29885afe91448ab
BuczkoBalazs <buci85@gmail.com>
2022-02-16 15:42:47 +0100

    most épp itt vagyunk.

--- frontend/script.js
+++ frontend/script.js
@@ -1,14 +1,14 @@
-/* console.log(a)
+/* 
+console.log(a)
 console.log(c)
- */
-
+*/
 /* 
 console.log("Hello!")
 console.log('Sima')
 console.log(`back
 tick`)
 
- */
+*/
 /* 
 console.log(typeof 2)
 console.log(typeof NaN)
@@ -24,7 +24,8 @@ console.log(null)
 console.log(typeof undefined)
 console.log(typeof null)
 
- *//* 
+*/
+/* 
 console.log({
     "title": "Start Coding",
     "type": "Solo",
@@ -47,8 +48,7 @@ console.log({
         }
     ]
 })
- */
-
+*/
 /* 
 var a = "Hello1"
 let b = "Hello2"
@@ -57,7 +57,7 @@ const c = "Hello3"
 console.log(a)
 console.log(b)
 console.log(c)
- */
+*/
 /* 
 function d(){
     if(1+1 === 2){
@@ -70,8 +70,7 @@ function d(){
     console.log(c)
 }
 d()
- */
-
+*/
 /* 
 let e = 1
 console.log(e)
@@ -80,8 +79,7 @@ console.log(e)
 e = e*2
 console.log(e)
 
- */
-
+*/
 /* 
 function multiplyByTwo(num){
     console.log(num)
@@ -91,7 +89,7 @@ let f = multiplyByTwo(1)
 console.log(f)
 console.log(multiplyByTwo(1))
 
- */
+*/
 /* 
 let g = {
     myMethod: function() {
@@ -100,7 +98,7 @@ let g = {
 }
 g.myMethod()
 
- */
+*/
 /* 
 let window = {
     addEventListener: function (eventName, eventFunction) {
@@ -111,8 +109,8 @@ let window = {
 }
 window.addEventListener("load", loadEvent)
 
- */
-
+*/
+/* 
 let h = {
     anotherMethod: function(text) {
         return `You sent the following argument to this method: ${text}`
@@ -120,3 +118,57 @@ let h = {
 }
 // let i = h.anotherMethod("argument")
 console.log(h.anotherMethod("argument"))
+
+*/
+/* 
+let myString = "mikkamakka";
+
+let mySecondString = myString;
+
+let myThirdString = "mikkamakka"
+
+console.log(myString);
+console.log(mySecondString);
+console.log(myString === mySecondString);
+console.log(myString === myThirdString);
+
+*/
+/* 
+let myObject = {
+    myString: "mikkamakka"
+};
+
+let = mySecondObject = myObject;
+
+let myThirdObject = {
+    myString: "mikkamakka"
+}
+
+mySecondObject.myString = "domdodom";
+
+let myFourthObject = {...myObject};
+
+myFourthObject.myString = "Vacskamati";
+
+console.log(myObject.myString);
+console.log(myFourthObject.myString);
+
+console.log(myObject === myThirdObject)
+
+console.log("" === "");
+console.log({} === {});
+
+*/
+/* 
+window.addEventListener("load", function() {
+    console.log("My first function")
+})
+
+*/
+
+function loadEvent() {
+    console.log("My second function")
+}
+window.addEventListener("load", loadEvent);
+
+
